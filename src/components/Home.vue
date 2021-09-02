afe1cbbc23feb187e7c0961f1506e009a31daba0
Thomas Lamers <thomas.lamers@iwelt.de>
2021-09-02 11:11:59 +0200

    some styling to store

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -8,19 +8,17 @@
     </div>
     <h2 class="text-2xl pb-2">Starter for Vue.js Development.</h2>
     <div class="grid">
-      <AdvertiseBox header="VUE 3.2.x" icon="pi-check-circle">
-
-      </AdvertiseBox>
-
-
-      <AdvertiseBox header="VITE 2.x" icon="pi-check-circle"/>
-      <AdvertiseBox header="PrimeVue 3.6.0" icon="pi-check-circle"/>
-      <AdvertiseBox header="PrimeFlex 3.x included" icon="pi-check-circle"/>
-      <AdvertiseBox header="PrimeIcons 2.x included" icon="pi-check-circle"/>
-      <AdvertiseBox header="Vue Router 4.x" icon="pi-check-circle"/>
-      <AdvertiseBox header="Pinia Store 2.x" icon="pi-check-circle"/>
-      <AdvertiseBox header="Script Setup" icon="pi-check-circle"/>
-      <AdvertiseBox header="PrimeBlocks Example" icon="pi-check-circle"/>
+      <AdvertiseBox header="VUE 3.2" icon="pi-check-circle" color="gray-700"/>
+      <AdvertiseBox header="Vue Router 4" icon="pi-check-circle" color="gray-700"/>
+      <AdvertiseBox header="Script Setup" icon="pi-check-circle" color="gray-700"/>
+      <AdvertiseBox header="Typescript 4.3" icon="pi-check-circle" color="blue-600"/>
+      <AdvertiseBox header="Vite 2" icon="pi-check-circle" color="blue-600"/>
+      <AdvertiseBox header="Pinia Store 2" icon="pi-check-circle"/>
+      <AdvertiseBox header="PrimeVue 3.7" icon="pi-check-circle" color="green-600"/>
+      <AdvertiseBox header="PrimeFlex 3 included" icon="pi-check-circle" color="green-600"/>
+      <AdvertiseBox header="PrimeIcons 2 included" icon="pi-check-circle" color="green-600"/>
+      <AdvertiseBox header="PrimeBlocks Example" icon="pi-check-circle" color="green-600"/>
+      <AdvertiseBox header="Tailwind CSS" icon="pi-check-circle"/>
     </div>
   </div>
 </template>
