f7135d519506b156f5acbcca28a14d76ac34a0c3
Thomas Lamers <thomas.lamers@iwelt.de>
2021-09-12 18:33:06 +0200

    advetise texts

--- src/components/AdvertiseBox.vue
+++ src/components/AdvertiseBox.vue
@@ -1,10 +1,12 @@
 <template>
   <div class="col-12 md:col-4 mb-4 px-5">
             <span class="p-3 shadow-2 mb-3 inline-block" style="border-radius: 10px">
-                <i :class="'pi ' + icon + ' text-4xl text-' + color"></i>
+                <i :class="'pi ' + icon + ' text-3xl text-' + color"></i>
             </span>
-    <div :class="'header-text text-900 mb-3 font-medium text-' + color">{{ header }}</div>
-    <slot/>
+    <div :class="'header-text mb-3 font-medium text-' + color">{{ header }}</div>
+    <div class="text-xs text-gray-600">
+      <slot/>
+    </div>
   </div>
 </template>
 
