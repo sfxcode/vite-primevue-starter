b2ad9bcc9d0f30ca2b0e18f783b885e81c4d9fff
sfxcode <tom@sfxcode.com>
2021-09-12 18:33:06 +0200

    advetise texts

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -6,25 +6,41 @@
       <span class="text-blue-400"> & TypeScript</span>
       <span class="text-blue-600"> & Vite</span>
     </div>
-    <h2 class="text-2xl pb-2">Starter for Vue.js Development.</h2>
+    <h2 class="text-2xl pb-6">Starter for Vue.js Development.</h2>
     <div class="grid">
-      <AdvertiseBox header="VUE 3.2" icon="pi-check-circle" color="gray-700"/>
-      <AdvertiseBox header="Vue Router 4" icon="pi-check-circle" color="gray-700"/>
-      <AdvertiseBox header="Script Setup" icon="pi-check-circle" color="gray-700"/>
-      <AdvertiseBox header="Typescript 4.3" icon="pi-check-circle" color="blue-600"/>
-      <AdvertiseBox header="Vite 2" icon="pi-check-circle" color="blue-600"/>
-      <AdvertiseBox header="Pinia Store 2" icon="pi-check-circle"/>
-      <AdvertiseBox header="PrimeVue 3.7" icon="pi-check-circle" color="green-600"/>
-      <AdvertiseBox header="PrimeFlex 3 included" icon="pi-check-circle" color="green-600"/>
-      <AdvertiseBox header="PrimeIcons 2 included" icon="pi-check-circle" color="green-600"/>
-      <AdvertiseBox header="PrimeBlocks Example" icon="pi-check-circle" color="green-600"/>
-      <AdvertiseBox header="Tailwind CSS" icon="pi-check-circle"/>
+      <AdvertiseBox header="VUE 3.2" icon="pi-check-circle" color="gray-700">
+        VueUse & VueUse Head included
+      </AdvertiseBox>
+      <AdvertiseBox header="Script Setup" icon="pi-check-circle" color="gray-700">
+        Reduce a lot of boilerplate code
+      </AdvertiseBox>
+      <AdvertiseBox header="Typescript 4.3" icon="pi-check-circle" color="blue-600">
+        Typesafe by default
+      </AdvertiseBox>
+      <AdvertiseBox header="Vite 2" icon="pi-check-circle" color="blue-600">
+        Vite Plugins like ViteIcons, PurgeIcons, ...
+      </AdvertiseBox>
+      <AdvertiseBox header="Vite Pages" icon="pi-check-circle" color="blue-600">
+        Routing by File/Folder & Vite Markdown included
+      </AdvertiseBox>
+      <AdvertiseBox header="Pinia Store 2" icon="pi-check-circle"  color="gray-600">
+        Replacement for VUEX Store
+      </AdvertiseBox>
+      <AdvertiseBox header="PrimeVue 3.7" icon="pi-check-circle" color="green-600">
+        Excellent Component Library for VUE
+      </AdvertiseBox>
+      <AdvertiseBox header="PrimeFlex 3" icon="pi-check-circle" color="green-600">
+        PrimeBlocks Example available (PrimeIcons 2 included)
+      </AdvertiseBox>
+      <AdvertiseBox header="Tailwind CSS" icon="pi-check-circle" color="blue-500">
+        Hero Icons included
+      </AdvertiseBox>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import AdvertiseBox from '../widgets/AdvertiseBox.vue';
+import AdvertiseBox from '../components/AdvertiseBox.vue';
 
 </script>
 
