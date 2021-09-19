4faccfb17aa4f421028f138c0e3eaf53a1513734
sfxcode <tom@sfxcode.com>
2021-09-19 21:11:36 +0200

    add patch-vue-directive-ssr because of primevue directives and ssr

---
+++ src/pages/store/Theme.vue
@@ -0,0 +1,18 @@
+<template>
+  <h5 class="font-bold text-2xl mb-4 text-blue-600">Pinia Store</h5>
+  <div class="grid grid-cols-2  gap-4">
+  <div class="col-span-2">Primevue Theme Store</div>
+  <div>Theme Name</div>
+  <div>{{ themeStore.themeName }}</div>
+
+  </div>
+</template>
+
+<script setup lang="ts">
+
+import {useThemeStore} from "@/store";
+
+const themeStore = useThemeStore();
+</script>
+
+<style scoped></style>
