8b703903a6944f096898a9da5ded58cb29ca3e93
sfxcode <tom@sfxcode.com>
2021-09-20 18:16:52 +0200

    add logging module

---
+++ src/modules/logging.ts
@@ -0,0 +1,11 @@
+import {UserModule} from '@/types'
+import {createLogger} from "vue-logger-plugin";
+
+const logger = createLogger({
+    enabled: true,
+    level: 'debug'
+})
+
+export const install: UserModule = ({app, router, isClient}) => {
+    app.use(logger)
+}
