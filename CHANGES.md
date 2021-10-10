4d64425b1d26b7ea558172936ebd7bd0bd568898
Thomas Lamers <thomas.lamers@iwelt.de>
2021-10-10 12:56:18 +0200

    dependency updates

--- CHANGES.md
+++ CHANGES.md
@@ -1,5 +1,7 @@
 # Changelog
 
+## 0.9.8 (2021-10-10)
+- use cypress for integration and component testing
 ## 0.9.0 (2021-09-13)
 
 - Many Vite Plugis added
--- cypress/support/index.js
+++ cypress/support/index.js
@@ -1,4 +1,3 @@
-/// <reference types="cypress" />
 // ***********************************************************
 // This example support/index.js is processed and
 // loaded automatically before your test files.
@@ -17,18 +16,5 @@
 // Import commands.js using ES2015 syntax:
 import './commands'
 
-Cypress.on('window:before:load', (win) => {
-  win.handleFromCypress = function (request) {
-    return fetch(request.url, {
-      method: request.method,
-      headers: request.requestHeaders,
-      body: request.requestBody,
-    }).then((res) => {
-      const content =
-        res.headers.map['content-type'] === 'application/json' ? res.json() : res.text()
-      return new Promise((resolve) => {
-        content.then((body) => resolve([res.status, res.headers, body]))
-      })
-    })
-  }
-})
+// Alternatively you can use CommonJS syntax:
+// require('./commands')
