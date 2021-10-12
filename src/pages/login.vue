faf34d5cff9847ffa6884f2710db63cebc19d60c
sfxcode <tom@sfxcode.com>
2021-10-12 21:09:00 +0200

    use netlify redirect

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -29,7 +29,7 @@
           <div class="flex items-center justify-between">
             <div class="flex items-center">
               <input id="remember-me" name="remember-me" type="checkbox" class="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded" />
-              <label for="remember-me" class="ml-2 block text-sm text-gray-900">
+              <label for="remember-me" class="ml-2 block text-sm">
                 Remember me
               </label>
             </div>
