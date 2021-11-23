c5cf34af38917bea9ab2f787aaf27237b065dc5a
Thomas Lamers <thomas.lamers@iwelt.de>
2021-11-23 11:15:35 +0100

    dependency updates

--- src/components/app/AppFooter.vue
+++ src/components/app/AppFooter.vue
@@ -1,11 +1,12 @@
 <template>
   <div class='layout-footer'>
-    <span class='font-medium ml-2'>Vite PrimeVue Starter</span>
+    <span class='font-medium ml-2'>Vite PrimeVue Starter - {{ version }}</span>
   </div>
 </template>
 
 <script setup lang='ts'>
 
+const version = ref(import.meta.env.VITE_APP_VERSION)
 </script>
 
 <style scoped>
