dca2de22e4a71d4cf2a31719c8c9d1779e494cca
Thomas Lamers <thomas.lamers@iwelt.de>
2022-01-27 20:47:40 +0100

    style(AdvertiseBox): some styling

--- src/components/AdvertiseBox.vue
+++ src/components/AdvertiseBox.vue
@@ -3,8 +3,8 @@
             <span class='p-3 shadow-2 mb-1 inline-block' style='border-radius: 10px'>
       <div :class="'i-mdi-check text-4xl text-' + color" />
             </span>
-    <div :class="'header-text mb-3 font-medium text-' + color">{{ header }}</div>
-    <div class='text-xs '>
+    <div :class="'mb-3 text-2xl text-' + color">{{ header }}</div>
+    <div class=''>
       <slot />
     </div>
   </div>
